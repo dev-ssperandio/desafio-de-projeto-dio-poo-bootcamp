9afb590890bc165c6705fbb4d16dd291a1041bc5
ssperandio <dev.ssperandio@gmail.com>
2024-08-02 18:57:42 -0300

    Implementando as classes Bootcamp e Dev

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -22,9 +24,42 @@ public class Main {
         mentoria.setDescricao("Descrição da mentoria de Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
 
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Develper");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devSidney = new Dev();
+        devSidney.setNome("Sidney");
+        devSidney.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Sidney: " + devSidney.getConteudosInscritos());
+        devSidney.progredir();
+        devSidney.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Sidney: " + devSidney.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Sidney: " + devSidney.getConteudosConcluidos());
+        System.out.println("XP: " + devSidney.calcularTotalXp());
+
+        System.out.println("------------------------------");
+
+        Dev devIvanessa = new Dev();
+        devIvanessa.setNome("Ivanessa");
+        devIvanessa.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Ivanessa: " + devIvanessa.getConteudosInscritos());
+        devIvanessa.progredir();
+        devIvanessa.progredir();
+        devIvanessa.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Sidney: " + devIvanessa.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Sidney: " + devIvanessa.getConteudosConcluidos());
+        System.out.println("XP: " + devIvanessa.calcularTotalXp());
+
     }
 }
